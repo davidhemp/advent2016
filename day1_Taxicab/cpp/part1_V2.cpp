c8fd105d67ef7627ecc1d18de60490e3963ada2a
david <cyber_veio@hotmail.com>
2017-01-08 15:00:49 +0000

    Move completed

--- day1_Taxicab/cpp/part1_V2.cpp
+++ day1_Taxicab/cpp/part1_V2.cpp
@@ -4,20 +4,11 @@
 #include <stdlib.h>
 #include <fstream>
 
-#include <sys/time.h>
-typedef unsigned long long timestamp_t;
-
-static timestamp_t
-get_timestamp ()
-{
-  struct timeval now;
-  gettimeofday (&now, NULL);
-  return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
-}
 using namespace std;
 
-int run(int& x, int& y){
-    timestamp_t t0 = get_timestamp();
+int main(){
+    int x = 0;
+    int y = 0;
     int coords[4] = {0, 0, 0, 0};
     signed int currentDirection = 0;
     std::string stepString;
@@ -47,32 +38,9 @@ int run(int& x, int& y){
         x = coords[1] - coords[3];
         y = coords[0] - coords[2];
         f.close();
+        printf("x: %i\ny: %i\n", x, y);
+        printf("Distance: %i\n", abs(x) + abs(y));
     } else {
-        cout << "Can not open file";
+        printf("Can not open file\n");
     }
-}
-
-int main(){
-    int x;
-    int y;
-    int counts = 10000;
-    double secs;
-    double ave = 0.0;
-    timestamp_t t0 = get_timestamp();
-    timestamp_t t1 = get_timestamp();
-    for (int i = 0; i < counts; ++i){
-        x = 0;
-        y = 0;
-        t0 = get_timestamp();
-        run(x, y);
-        t1 = get_timestamp();
-        secs = (t1 - t0) / 1000000.0L;
-        ave += secs;
-    }
-    // output solution
-    cout << "x: " << x << endl;
-    cout << "y: " << y << endl;
-    cout << "Distance: " << abs(x) + abs(y) << endl;
-    ave = ave/counts;
-    cout << ave << " seconds" << endl;
 }
